abfd121f7e22af4d3f69e005e923f8576c1a7cb3
Tracer1337 <merlin.moelter@gmail.com>
2022-06-08 20:43:26 +0200

    Implement cube rotations

--- rubiks-cube/Cube.js
+++ rubiks-cube/Cube.js
@@ -4,6 +4,10 @@ import Stats from "stats"
 import Cubie from "./Cubie.js"
 
 class Cube {
+    constructor() {
+        this.cube = []
+    }
+    
     init() {
         this.scene = new THREE.Scene()
 
@@ -37,6 +41,13 @@ class Cube {
         document.body.appendChild(this.stats.dom)
 
         this.createCubies()
+
+        this.cube[0].highlight(0x00ff00)
+        this.cube[2].highlight(0x0000ff)
+
+        console.log(this)
+
+        document.addEventListener("keydown", this.handleKeyDown.bind(this))
     
         this.animate()
     }
@@ -50,15 +61,85 @@ class Cube {
     }
 
     createCubies() {
+        let index = 0
         for (let x = -1; x <= 1; x++) {
             for (let y = -1; y <= 1; y++) {
                 for (let z = -1; z <= 1; z++) {
                     const matrix = new THREE.Matrix4()
                     matrix.makeTranslation(x, y, z)
-                    const cubie = new Cubie(matrix)
+                    const cubie = new Cubie(matrix, x, y, z)
                     cubie.addToScene(this.scene)
+                    this.cube[index++] = cubie
                 }
             }
+        }
+    }
+
+    rotateX(index) {
+        this.cube
+            .filter((qb) => qb.x === index)
+            .forEach((qb) => {
+                const matrix = new THREE.Matrix3()
+                matrix.translate(qb.y, qb.z)
+                matrix.rotate(Math.PI / 2)
+                qb.update(
+                    qb.x,
+                    Math.round(matrix.elements[6]),
+                    Math.round(matrix.elements[7])
+                )
+            })
+    }
+
+    rotateY(index) {
+        this.cube
+            .filter((qb) => qb.y === index)
+            .forEach((qb) => {
+                const matrix = new THREE.Matrix3()
+                matrix.translate(qb.x, qb.z)
+                matrix.rotate(Math.PI / 2)
+                qb.update(
+                    Math.round(matrix.elements[6]),
+                    qb.y,
+                    Math.round(matrix.elements[7])
+                )
+            })
+    }
+
+    rotateZ(index) {
+        this.cube
+            .filter((qb) => qb.z === index)
+            .forEach((qb) => {
+                const matrix = new THREE.Matrix3()
+                matrix.translate(qb.x, qb.y)
+                matrix.rotate(Math.PI / 2)
+                qb.update(
+                    Math.round(matrix.elements[6]),
+                    Math.round(matrix.elements[7]),
+                    qb.z
+                )
+            })
+    }
+
+    handleKeyDown(event) {
+        switch(event.key) {
+            case "x":
+                this.rotateX(-1)
+                break
+            case "X":
+                this.rotateX(1)
+                break
+            case "y":
+                this.rotateY(-1)
+                break
+            case "Y":
+                this.rotateY(1)
+                break
+            case "z":
+                this.rotateZ(-1)
+                break
+            case "Z":
+                this.rotateZ(1)
+                break
         }
     }
 }
--- rubiks-cube/Cubie.js
+++ rubiks-cube/Cubie.js
@@ -1,7 +1,11 @@
 import * as THREE from "three"
 
 class Cubie {
-    constructor(matrix) {
+    constructor(matrix, x, y, z) {
+        this.matrix = matrix
+        this.x = x
+        this.y = y
+        this.z = z
         this.material = new THREE.MeshLambertMaterial()
         const size = 0.98
         this.geometry = new THREE.BoxGeometry(size, size, size)
@@ -12,6 +16,20 @@ class Cubie {
 
     addToScene(scene) {
         scene.add(this.mesh)
+    }
+
+    highlight(color) {
+        this.material.color = new THREE.Color(color)
+    }
+
+    update(x, y, z) {
+        this.mesh.matrix = new THREE.Matrix4()
+        this.matrix.identity()
+        this.matrix.makeTranslation(x, y, z)
+        this.mesh.applyMatrix4(this.matrix)
+        this.x = x
+        this.y = y
+        this.z = z
     }
 }
 
